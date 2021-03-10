564c71da3e4b8235825af8dda10781a047a5256e
tmprakash3 <tammineni.maniprakash@gmail.com>
2021-03-10 07:11:16 +0530

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from '../lib/apolloClient';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const apolloClient = useApollo(pageProps.initialApolloState);
+  return (
+    <ApolloProvider client={apolloClient}>
+      <div style={{ margin: '20px' }}>
+        <Component {...pageProps} />
+      </div>
+    </ApolloProvider>
+  );
 }
 
 export default MyApp
---
+++ pages/components/playersList.js
@@ -0,0 +1,84 @@
+import { gql, useQuery } from '@apollo/client';
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: "inline-block",
+        margin: "0 2px",
+        transform: "scale(0.8)",
+    },
+    title: {
+        fontSize: 18,
+    },
+    pos: {
+        marginBottom: 12,
+        fontSize: 12,
+    },
+});
+
+// const classes = useStyles();
+
+export const ALL_PLAYERS_QUERY = gql`
+    query allPlayers{
+        queryPlayer {
+            name
+            position
+            country {
+                id
+                name
+                stadium
+            }
+            club {
+                id
+                name
+                stadium
+            }
+            id
+        }
+    }
+`;
+
+export default function PlayersList() {
+    const { loading, error, data } = useQuery(ALL_PLAYERS_QUERY);
+    if (error)
+        return <div>Error while loading the data</div>;
+    if (loading)
+        return <div>Loading...</div>;
+    console.log(data);
+    const { queryPlayer: allPlayers } = data;
+    return (
+        <Grid style={{ marginTop: '20px' }} container spacing={2}>
+            {allPlayers.map((player) => (
+                <Grid item xs={4} key={player.id}>
+                    <Card className={classes.root}>
+                        <CardContent>
+                            <Typography
+                                className={classes.title}
+                                color="textPrimary"
+                                gutterBottom
+                            >
+                                {player.name}
+                            </Typography>
+                            <Typography className={classes.pos} color="textSecondary">
+                                {player.club.name}
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                Position - {player.position}
+                                <br />
+                    Country - {player.country.name}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            ))}
+        </Grid>
+    )
+}
